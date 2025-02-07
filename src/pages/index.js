ea115e420589847cbb6b1436a0b0690f5da2c020
Jones Beach <jonesbeach60@gmail.com>
2025-02-07 09:13:21 -0500

    Actually use the Layout and Seo properly

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,31 @@
 import React from "react"
 import * as styles from "./index.module.css"
+import Seo from "../components/seo"
+import Layout from "../components/layout"
 
 const HomePage = () => {
   return (
-    <div class={styles.container}>
-      <head>
-        <title>Coming Soon - From Scratch Press</title>
-      </head>
-      <h1 class={styles.title}>From Scratch Press</h1>
-      <p class={styles.subtitle}>Coming soon! Subscribe below for updates.</p>
-      <iframe
-        title="Substack"
-        src="https://fromscratchdotorg.substack.com/embed"
-        width="480"
-        height="320"
-        style={{ border: "1px solid #EEE", background: "white" }}
-        frameborder="0"
-      ></iframe>
-      <p class={styles.footer}>
-        Visit <a href="https://fromscratchcode.com/">From Scratch Code</a> for
-        personalized Rust and Python mentorship.
-      </p>
-    </div>
+    <Layout>
+      <div class={styles.container}>
+        <h1 class={styles.title}>From Scratch Press</h1>
+        <p class={styles.subtitle}>Coming soon! Subscribe below for updates.</p>
+        <iframe
+          title="Substack"
+          src="https://fromscratchdotorg.substack.com/embed"
+          width="480"
+          height="320"
+          style={{ border: "1px solid #EEE", background: "white" }}
+          frameborder="0"
+        ></iframe>
+        <p class={styles.footer}>
+          Visit <a href="https://fromscratchcode.com/">From Scratch Code</a> for
+          personalized Rust and Python mentorship.
+        </p>
+      </div>
+    </Layout>
   )
 }
+
+export const Head = () => <Seo title="Coming Soon" />
 
 export default HomePage
