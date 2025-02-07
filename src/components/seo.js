9e4099e2a325cec7eab2647251cd70784c85ef5e
Jones Beach <jonesbeach60@gmail.com>
2025-02-07 08:36:33 -0500

    Add Open Graph image

--- src/components/seo.js
+++ src/components/seo.js
@@ -25,6 +25,8 @@ function Seo({ description, title, children }) {
 
   const metaDescription = description || site.siteMetadata.description
   const defaultTitle = site.siteMetadata?.title
+  // This file is served directly from static/, NOT managed by Gatsby. (Same as the favicon.)
+  const og_banner_image = "/og_banner_image.png"
 
   return (
     <>
@@ -33,6 +35,7 @@ function Seo({ description, title, children }) {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
+      <meta property="og:image" content={og_banner_image} />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
       <meta name="twitter:title" content={title} />
